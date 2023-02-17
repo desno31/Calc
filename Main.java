6d14ee989728c22ff3ea4047da1ad0bfc37f4743
desno31 <desno31@gmail.com>
2023-02-17 15:44:15 +0300

    test
    test

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Main {
        static int romanToInt(String s){
            switch (s){
@@ -116,5 +117,18 @@ public class Main {
         result = intToRoman(value);
         }
         return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean b = true;
+        while (b == true) {
+            if (sc.hasNext("stop")){
+                b = false;
+                break;
+            }
+            String input = sc.nextLine();
+            System.out.println(calc(input));
+        }
     }
 }
